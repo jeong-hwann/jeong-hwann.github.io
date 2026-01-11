48470e625aaf2e82b0c5565d8872201c59bec74f
jeong-hwann <jang990608@gmail.com>
2026-01-11 14:29:53 +0000

    Deploying to gh-pages from @ jeong-hwann/jeong-hwann.github.io@23898dc14b4f5d562700df643b997266f2db611f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -462,11 +462,11 @@ ninja.data = [{
           window.open("mailto:%6A%61%6E%67%39%39%30%36%30%38@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/정환-장-709987387", "_blank");
         },
       },{
       id: 'light-theme',

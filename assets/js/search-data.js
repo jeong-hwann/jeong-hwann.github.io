90876a9c5b3a636023a6b3457aacfe2bd1865d19
jeong-hwann <jang990608@gmail.com>
2026-01-11 15:32:20 +0000

    Deploying to gh-pages from @ jeong-hwann/jeong-hwann.github.io@faecbbe8794272caf52c9c188bbe823c27063f53 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -409,12 +409,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-4-dof-legged-robot",
+          section: "News",},{id: "projects-4-dof-legged-robot",
           title: '4-DoF Legged Robot',
           description: "4-DoF legged robot using differential gears mechanism",
           section: "Projects",handler: () => {

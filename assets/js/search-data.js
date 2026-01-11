3a4ae2f4beadaa8a0278293984296ac076813a5a
jeong-hwann <jang990608@gmail.com>
2026-01-11 15:23:31 +0000

    Deploying to gh-pages from @ jeong-hwann/jeong-hwann.github.io@fb954c1761a6043c3e6365d0ef808cf40b91d081 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -414,11 +414,6 @@ ninja.data = [{
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
             },},{id: "projects-4-dof-legged-robot",
           title: '4-DoF Legged Robot',
           description: "4-DoF legged robot using differential gears mechanism",
